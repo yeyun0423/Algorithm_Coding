77778a4771d963587d35eabf93d018c33a25bde2
yeyun0423 <168630116+yeyun0423@users.noreply.github.com>
2026-02-02 01:40:02 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
@@ -6,19 +6,15 @@ int main()
     ios_base::sync_with_stdio(0); 
     cin.tie(0);
     string line;
-    
     while(1){
         getline(cin,line);
-        if(line=="#"){
-            break;
-        }
+        if(line=="#") break;
         int count=0;
-        for(char c : line ){
-            if(c>='A'&& c<='Z'){
+        for(char c:line){
+            if(c>='A'&&c<='Z'){
                 c=c-'A'+'a';
             }
-            if (c == 'a' || c == 'e' || c == 'i' ||
-                c == 'o' || c == 'u') {
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
                 count++;
             }
         }
