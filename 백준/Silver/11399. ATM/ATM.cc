773844fc4626fe4891f654373cfa34f2ebdd9586
yeyun0423 <168630116+yeyun0423@users.noreply.github.com>
2026-02-05 00:35:32 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+    
+    vector<int> time(n);
+    for (int i = 0; i < n; i++) {
+        cin >> time[i];
+    }
+    
+    sort(time.begin(),time.end());
+    
+    int progressSum=0;
+    int totalSum=0;
+    
+    for(int i=0;i<n;i++){
+        progressSum+=time[i];
+        totalSum +=progressSum;
+    }
+    
+    cout<<totalSum;
+    
+    return 0;
+}
